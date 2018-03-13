1768857a5f42a7388bc846a63a28ede75db19d48
jordanrj <jordanrjones33@gmail.com>
2018-03-12 21:54:15 -0700

    Basic layout complete adn responsive.

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -1,18 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  contructor() {
+    this.state = { 
+      numbers: [],
+      operations: [],
+      result: 0,
+      display: ""
+    }
+  }
+
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1 className="calcHeader">React Calculator</h1>
+        <div className="calculator">
+          <h2 className="calcDisplay">Results go here</h2>
+          <div className="calcInputZone">
+            <div className="inputClear calcInput">C</div>
+            <div className="inputNegative calcInput">+/-</div>
+            <div className="inputDivide calcInput">/</div>
+            <div className="inputMultiply calcInput">X</div>
+            <div className="inputSeven calcInput">7</div>
+            <div className="inputEight calcInput">8</div>
+            <div className="inputNine calcInput">9</div>
+            <div className="inputSubtract calcInput">-</div>
+            <div className="inputFour calcInput">4</div>
+            <div className="inputFive calcInput">5</div>
+            <div className="inputSix calcInput">6</div>
+            <div className="inputAdd calcInput">+</div>
+            <div className="inputOne calcInput">1</div>
+            <div className="inputTwo calcInput">2</div>
+            <div className="inputThree calcInput">3</div>
+            <div className="inputEquals calcInput">=</div>
+            <div className="inputZero calcInput">0</div>
+            <div className="inputDecimal calcInput">.</div>            
+          </div>
+        </div>
       </div>
     );
   }
